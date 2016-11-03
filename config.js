94a2fac932054edeb3a2aea99aee513675fb4fc8
Prudhvi Dharmavarapu <Prudhvi Dharmavarapu>
2016-11-03 16:48:15 +0530

    added db auth

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ var configs = {};
 configs.applicationPort = 3000;
 configs.dbName = 'CrossoverVideosAssignment';
 configs.dbHost = 'localhost';
-configs.live = 'mongodb://<dbuser>:<dbpassword>@ds057066.mlab.com:57066/portal';
+configs.live = 'mongodb://portal:portal@ds057066.mlab.com:57066/portal';
 
 module.exports = configs;
